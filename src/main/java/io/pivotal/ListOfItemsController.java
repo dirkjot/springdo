ce2df0ddda289dc1c5d33b6445fe20bc80f39d42
Navya <ncanumalla@pivotal.io>
2015-09-21 13:48:28 -0700

    First test and simple implementation on story #103828032

---
+++ src/main/java/io/pivotal/ListOfItemsController.java
@@ -0,0 +1,35 @@
+package io.pivotal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by pivotal on 9/21/15.
+ */
+
+@RestController
+public class ListOfItemsController {
+
+    @RequestMapping("/")
+    List simpleminded() {
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+        Map<String, String> map1 = new HashMap<String, String>();
+        map1.put("id", "1");
+        map1.put("title", "Go for a swim");
+        map1.put("content", "Go swimming on Monday night");
+        result.add(0, map1);
+
+        Map<String, String> map2 = new HashMap<String, String>();
+        map2.put("id", "2");
+        map2.put("title", "Visit farmer's market");
+        map2.put("content", "Buy dairy and eggs at farmers market on Wednesday");
+        result.add(1, map2);
+
+        return result;
+    }
+}
---
+++ src/test/java/io/pivotal/ListOfItemsControllerTest.java
@@ -0,0 +1,61 @@
+package io.pivotal;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringdoApplication.class)
+@WebAppConfiguration
+public class ListOfItemsControllerTest {
+
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+
+    @Before
+    public void setUp() throws Exception {
+        /* do not use standAloneSetup, as it is not good for web apps */
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void whenDefaultPlaceholdersLoadedTwoTasksShouldShow() throws Exception {
+        // note that we are only testing spring json output, we are assuming angular shows this \
+        // nicely on the user's page
+
+        // we assume that our first version always has two tasks:
+        // id1 / title: Go for a swim  / Content:  Go swimming on Monday night
+        // id2 / title: Visit farmer's market / Content: Buy dairy and eggs at farmers market on Wednesday
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                //.andExpect(view().name("list_of_items"))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is("1")))
+                .andExpect(jsonPath("$[1].id", is("2")))
+                .andExpect(jsonPath("$[0].title", containsString("swim")))
+                .andExpect(jsonPath("$[1].title", containsString("market")))
+                .andExpect(jsonPath("$[0].content", containsString("swimming on Monday")))
+                .andExpect(jsonPath("$[1].content", containsString("dairy and eggs")));
+    }
+
+}
