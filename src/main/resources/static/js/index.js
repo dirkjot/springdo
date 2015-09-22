32a835b39876dfbec45c6b3d35541790fdbd743e
Navya <ncanumalla@pivotal.io>
2015-09-22 15:13:11 -0700

    front end changes for editing todo items complete

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 angular.module('index', []).controller('home', function($scope, $http) {
 
 	$http.get('resource/list/').success(function(data) {
+		//$scope.greeting = data;
+		//$scope.greeting = {id: 'xxx', content: 'Hello World!'};
 		$scope.listofitems = data; });
 
 	$scope.onDoneClick = function (item) {
@@ -9,13 +11,35 @@ angular.module('index', []).controller('home', function($scope, $http) {
 				      function (failure) { console.log("backend error", failure) });
 		};
 
+	// hiding & showing content
 	$scope.content = [];
+
 	$scope.toggleContent = function(itemid) {
+		$scope.editorEnabled = false;
 		for (var key in $scope.content) {
-			if (key != itemid && $scope.content[key]) {
-				$scope.content[key] = false;
-			}
-		}
-		$scope.content[itemid] = !$scope.content[itemid];
+			if (key != itemid) {
+				$scope.content[key] = false; }}
+		$scope.content[itemid] = !$scope.content[itemid]; };
+
+
+	// editing content
+	$scope.editorEnabled = false;
+
+	$scope.goedit = function (item) {
+		$scope.editorEnabled = true;
+		item.titleField = item.title;
+		item.contentField = item.content;
 	};
+
+	$scope.saveedit = function (item) {
+		item.title = item.titleField;
+		item.content = item.contentField;
+		$scope.editorEnabled = false;
+		// TODO post this stuff
+		// post('/resource/save/<id>/title/content/done/')
+
+	};
+
+
+
 });
