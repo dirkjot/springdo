4e04ba01f5b7f9cf0dbdb1c4e4ae107eee899b63
Navya <ncanumalla@pivotal.io>
2015-09-23 13:55:06 -0700

    added a test for the save end point

--- src/main/java/io/pivotal/SpringdoApplication.java
+++ src/main/java/io/pivotal/SpringdoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class SpringdoApplication implements CommandLineRunner {
@@ -18,7 +19,7 @@ public class SpringdoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        // save a couple of customers
+        // save a couple of fake to do items
         itemRepository.save(new Item("Jack", "Bauer lorem ipsum and a very long story indeed. Bauer lorem ipsum and a very long story indeed.  Bauer lorem ipsum and a very long story indeed.  Bauer lorem ipsum and a very long story indeed. "));
         itemRepository.save(new Item("Chloe", "O'Brian"));
         itemRepository.save(new Item("Kim", "Bauer"));
@@ -27,3 +28,4 @@ public class SpringdoApplication implements CommandLineRunner {
     }
 
 }
+
