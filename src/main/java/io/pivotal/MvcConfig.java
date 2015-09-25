ec3908d9b6844b81e2a020b0c992c5f293575108
Navya <ncanumalla@pivotal.io>
2015-09-24 17:29:59 -0700

    Login with a default username and password added. Logout added as a link to separate thymeleaf template because thymeleaf does not play well with angularjs

---
+++ src/main/java/io/pivotal/MvcConfig.java
@@ -0,0 +1,19 @@
+package io.pivotal;
+
+/**
+ * Created by pivotal on 9/24/15.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logoutbutton").setViewName("logoutbutton");
+    }
+
+}
