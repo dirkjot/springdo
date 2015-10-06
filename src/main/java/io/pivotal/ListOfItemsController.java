1d527951050058876bb98ea579ea768954bcab89
Dirk Janssen <djanssen@pivotal.io>
2015-10-05 17:59:34 -0700

    final state at springdo-2

--- src/main/java/io/pivotal/ListOfItemsController.java
+++ src/main/java/io/pivotal/ListOfItemsController.java
@@ -1,18 +1,35 @@
 package io.pivotal;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Created by pivotal on 10/1/15.
+ * Created by pivotal on 9/21/15.
  */
+
 @RestController
 public class ListOfItemsController {
 
-    @RequestMapping(value = "/", method= RequestMethod.GET)
-    public String mainPage() {
-        return "<html><body><h1>List of ToDO items</h1></body></html>";
+    @RequestMapping("/resource/list")
+    List simpleminded() {
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+        Map<String, String> map1 = new HashMap<String, String>();
+        map1.put("id", "1");
+        map1.put("title", "Go for a swim");
+        map1.put("content", "Go swimming on Monday night");
+        result.add(0, map1);
+
+        Map<String, String> map2 = new HashMap<String, String>();
+        map2.put("id", "2");
+        map2.put("title", "Visit farmer's market");
+        map2.put("content", "Buy dairy and eggs at farmers market on Wednesday");
+        result.add(1, map2);
+
+        return result;
     }
 }
