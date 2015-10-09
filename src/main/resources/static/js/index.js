21bc0893b1e512ed5df653e41e9916fd77067558
Dirk Janssen <djanssen@pivotal.io>
2015-10-09 10:24:31 -0700

    Added javascript to show and hide content, step 1

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,16 @@
-angular.module('index', []).
-    controller('home', function($scope, $http) {
-        $http.get('resource/list/').then(function(success) {
-            $scope.listofitems = success.data;
-        });
-        $scope.onDoneClick = function (item) {
-            console.log(item.done);
-            $http.post('/resource/done/' + item.id + '/' + item.done + '/')
-                .then(function (success) { console.log("backend received done state change", success) },
-                      function (failure) { console.log("backend error", failure) });
-        };
-    });
+angular.module('index', []).controller('home', function($scope, $http) {
+
+	$http.get('resource/list/').success(function(data) {
+		$scope.listofitems = data; });
+
+	$scope.onDoneClick = function (item) {
+			$http.post('/resource/done/' + item.id + '/' + item.done + '/')
+				.then(function (success) { console.log("backend received done state change", success) },
+				      function (failure) { console.log("backend error", failure) });
+		};
+
+	$scope.content = [];
+	$scope.toggleContent = function(itemid) {
+		$scope.content[itemid] = !$scope.content[itemid];
+	};
+});
