347d6233cb9310f207a5b6d403a82be569caaa4f
Michael Oleske <moleske@pivotal.io>
2016-03-17 15:47:47 -0700

    Add CSRF protection and a attack html example

--- src/main/java/io/pivotal/WebSecurityConfig.java
+++ src/main/java/io/pivotal/WebSecurityConfig.java
@@ -10,6 +10,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Configuration
@@ -19,23 +33,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/js/**").permitAll()
-                    .antMatchers("/css/**").permitAll()
-                    .antMatchers("/fonts/**").permitAll()
-                    .antMatchers("/who/").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+        http.authorizeRequests()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/fonts/**").permitAll()
+                .antMatchers("/who/").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/credentials.html")
-                    .permitAll()
-                    .and()
+                .loginPage("/credentials.html")
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll()
-                    .and()
-                .csrf().disable()  // not great
-        ;
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();  // not great
+//                .csrfTokenRepository(csrfTokenRepository())
+//                .and()
+//                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
     }
 
     @Override
@@ -44,8 +60,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public UserManager myUserManager () {
+    public UserManager myUserManager() {
         return new UserManager();
+    }
+
+    private Filter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+                CsrfToken csrfToken = (CsrfToken) httpServletRequest.getAttribute(CsrfToken.class.getName());
+                if(csrfToken != null) {
+                    Cookie cookie = WebUtils.getCookie(httpServletRequest, "XSRF-TOKEN");
+                    String token = csrfToken.getToken();
+                    if(cookie==null || token != null && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");  //probably don't want that normally
+                        httpServletResponse.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+            }
+        };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
     }
 }
 
